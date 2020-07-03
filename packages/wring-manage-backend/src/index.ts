ee2607102ebca9416a6326debe137374a0a43322
Bill Cauchois <wcauchois@gmail.com>
2020-07-03 17:40:05 -0400

    basic api server functionality

--- packages/wring-manage-backend/src/index.ts
+++ packages/wring-manage-backend/src/index.ts
@@ -1,14 +1,68 @@
 import { ApolloServer, gql } from "apollo-server";
+import findRoot from "find-root";
+import fs = require('fs');
+import path = require('path');
+import { GraphQLJSON } from 'graphql-scalars';
+
+const packageDirectory = findRoot(process.cwd());
+const configName = `web-ring.json`;
+const configPath = path.join(packageDirectory, configName);
 
 const typeDefs = gql`
+  scalar JSON
+
+  type WebRingConfig {
+    id: ID!
+    data: JSON!
+  }
+
   type Query {
-    hi: String
+    config: WebRingConfig
+  }
+
+  type Mutation {
+    writeConfig(data: JSON!): WebRingConfig!
   }
 `;
 
+class WebRingConfig {
+  readonly data: any;
+
+  constructor(data: any) {
+    this.data = data;
+  }
+
+  get id() {
+    return 'config';
+  }
+
+  static fromFile(path: string) {
+    if (fs.existsSync(path)) {
+      const config = JSON.parse(fs.readFileSync(path, { encoding: 'utf-8' }));
+      return new WebRingConfig(config);
+    } else {
+      return null;
+    }
+  }
+
+  write(path: string) {
+    fs.writeFileSync(path, JSON.stringify(this.data, null, 2));
+  }
+}
+
 const resolvers = {
+  JSON: GraphQLJSON,
+
   Query: {
-    hi: () => "hi!!"
+    config: () => WebRingConfig.fromFile(configPath)
+  },
+
+  Mutation: {
+    writeConfig(_mutation: any, { data }: { data: any }) {
+      const config = new WebRingConfig(data);
+      config.write(configPath);
+      return config;
+    }
   }
 };
 
