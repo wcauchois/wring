a9608b378a393b7f316e3ac0b0c0653921a5a816
Bill Cauchois <wcauchois@gmail.com>
2020-07-03 17:27:31 -0400

    skeleton graphql server

--- packages/wring-manage-backend/src/index.ts
+++ packages/wring-manage-backend/src/index.ts
@@ -1,1 +1,25 @@
-console.log('hi');
+import { ApolloServer, gql } from "apollo-server";
+
+const typeDefs = gql`
+  type Query {
+    hi: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hi: () => "hi!!"
+  }
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+});
+
+const DEFAULT_PORT = 4667;
+const port = process.env.PORT ? parseInt(process.env.PORT) : DEFAULT_PORT;
+
+server.listen(port).then(({ url }) => {
+  console.log(`Server ready at ${url}`);
+});
